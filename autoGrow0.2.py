bd22c9cf6aa8c46fb8268699062343816eb2a10c
Sp0Raw Laptop <letun9@mail.ru>
2019-02-07 16:14:53 +0500

    add init searchSensor

--- autoGrow0.2.py
+++ autoGrow0.2.py
@@ -363,14 +363,12 @@ class BoxClimate:
     try:
       data = json.loads(self.sensArray)
       self.countSensor = data["sens_count"]
-      print (self.countSensor)
-      time.sleep(1)
-      #simpleList = []
-      # for x in range(0, self.countSensor):
-      #   #simpleList.append(TemperatureSensor(x))
-      #   obj = TemperatureSensor(int(x))
-      #   print obj
-      #   self.sensArrayXXX.append(obj)
+      # print (self.countSensor)
+      # time.sleep(1)
+      for x in range(0, self.countSensor):
+        obj = TemperatureSensor(int(x))
+        print obj
+        self.sensArrayXXX.append(obj)
     except:
       print("Error on parsing json" + self.sensArray)
 
